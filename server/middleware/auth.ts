c53f4ad32a1527797386f8e9917d316e7d79fd35
Jan Poonthong <janpoonthong628@gmail.com>
2023-10-31 09:30:12 +0700

    feat: add roles authorization

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -37,3 +37,18 @@ export const isAuthenticated = CatchAsyncError(
     next();
   },
 );
+
+// Validate user role
+export const authorizeRoles = (...roles: string[]) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    if (!roles.includes(req.user?.role || "")) {
+      return next(
+        new ErrorHandler(
+          `Role: ${req.user?.role} is not allowed to access this resource`,
+          403,
+        ),
+      );
+    }
+    next();
+  };
+};
