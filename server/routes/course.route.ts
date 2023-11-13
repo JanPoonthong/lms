70927c9a2ab0df6bc6594e9e4c424457733296d5
Jan Poonthong <janpoonthong628@gmail.com>
2023-11-13 09:15:58 +0700

    feat: add edit course to route

--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { uploadCourse } from "../controllers/course.controller";
+import { uploadCourse, editCourse } from "../controllers/course.controller";
 
 import { isAuthenticated, authorizeRoles } from "../middleware/auth";
 
@@ -12,3 +12,12 @@ courseRouter.post(
   authorizeRoles("admin"),
   uploadCourse,
 );
+
+courseRouter.put(
+  "/edit-course/:id",
+  isAuthenticated,
+  authorizeRoles("admin"),
+  editCourse,
+);
+
+export default courseRouter;
